b618cd80bd93a6a9e7c71dca28fbb3ba17332d29
Marian <marianmrva123@gmail.com>
2019-10-11 13:37:50 +0200

    Updates

---
+++ precache-manifest.2f52020f55b5bfedce25a79c235ece5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3eab06d4902a6e53e9c54e493abd009",
+    "url": "/react-twitchify/index.html"
+  },
+  {
+    "revision": "348ae80863b591465ec3",
+    "url": "/react-twitchify/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "9d1361645d447b9efbb4",
+    "url": "/react-twitchify/static/css/main.b7dce744.chunk.css"
+  },
+  {
+    "revision": "348ae80863b591465ec3",
+    "url": "/react-twitchify/static/js/2.ef5e1973.chunk.js"
+  },
+  {
+    "revision": "9d1361645d447b9efbb4",
+    "url": "/react-twitchify/static/js/main.4236cdf2.chunk.js"
+  },
+  {
+    "revision": "61d726208955b52548df",
+    "url": "/react-twitchify/static/js/runtime-main.d5dc8f35.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.22b66f00710338b07ccf374f27397178.js"
+  "/react-twitchify/precache-manifest.2f52020f55b5bfedce25a79c235ece5d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-twitchify/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
